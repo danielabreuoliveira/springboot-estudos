f07d644cd7a97e7dc0542e8b6e2ebc3c04ad48f8
Daniel de Abreu Oliveira <danieloliveirabsi@gmail.com>
2026-06-28 16:07:58 -0300

    Primeiro projeto de CRUD em  Spring Boot

---
+++ src/main/java/app/service/CarroService.java
@@ -0,0 +1,41 @@
+package app.service;
+
+import app.entity.Carro;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import app.repository.CarroRepository;
+
+import java.util.List;
+
+@Service
+public class CarroService {
+
+    @Autowired
+    private CarroRepository carroRepository;
+
+    public String save(Carro carro){
+        this.carroRepository.save(carro);
+        return "Carro salvo com sucesso!";
+    }
+
+    public String update(Carro carro, long id){
+        carro.setId(id);
+        this.carroRepository.save(carro);
+        return "Carro foi atualizado com sucesso!";
+    }
+
+    public String delete(long id){
+        this.carroRepository.deleteById(id);
+        return "Carro deletado com  sucesso!";
+    }
+
+    public List<Carro> findAll(){
+        List<Carro> lista = this.carroRepository.findAll();
+        return lista;
+    }
+
+    public Carro findById(long  id){
+        Carro carro = this.carroRepository.findById(id).get();
+        return carro;
+    }
+}
